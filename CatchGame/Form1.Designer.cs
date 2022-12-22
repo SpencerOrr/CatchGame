f4aa89dc4f1fc9a460a8ba47409e0b370e1e1916
Spencer Orr <spenorr550@ed.amdsb.ca>
2022-12-21 21:11:27 -0500

    space race

--- CatchGame/Form1.Designer.cs
+++ CatchGame/Form1.Designer.cs
@@ -29,65 +29,106 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.timeLabel = new System.Windows.Forms.Label();
-            this.scoreLabel = new System.Windows.Forms.Label();
             this.gameLoop = new System.Windows.Forms.Timer(this.components);
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subTitlelabel = new System.Windows.Forms.Label();
+            this.hero2Points = new System.Windows.Forms.Label();
+            this.hero1Points = new System.Windows.Forms.Label();
+            this.winLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // timeLabel
-            // 
-            this.timeLabel.BackColor = System.Drawing.Color.Transparent;
-            this.timeLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.timeLabel.ForeColor = System.Drawing.Color.White;
-            this.timeLabel.Location = new System.Drawing.Point(12, 24);
-            this.timeLabel.Name = "timeLabel";
-            this.timeLabel.Size = new System.Drawing.Size(100, 23);
-            this.timeLabel.TabIndex = 0;
-            this.timeLabel.Text = "timeLabel";
-            // 
-            // scoreLabel
-            // 
-            this.scoreLabel.BackColor = System.Drawing.Color.Transparent;
-            this.scoreLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.scoreLabel.ForeColor = System.Drawing.Color.White;
-            this.scoreLabel.Location = new System.Drawing.Point(488, 24);
-            this.scoreLabel.Name = "scoreLabel";
-            this.scoreLabel.Size = new System.Drawing.Size(100, 23);
-            this.scoreLabel.TabIndex = 1;
-            this.scoreLabel.Text = "scoreLabel";
-            this.scoreLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // gameLoop
             // 
             this.gameLoop.Enabled = true;
-            this.gameLoop.Interval = 20;
+            this.gameLoop.Interval = 60;
             this.gameLoop.Tick += new System.EventHandler(this.gameLoop_Tick);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.IndianRed;
+            this.titleLabel.Location = new System.Drawing.Point(140, 67);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(130, 31);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Title label";
+            // 
+            // subTitlelabel
+            // 
+            this.subTitlelabel.AutoSize = true;
+            this.subTitlelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTitlelabel.ForeColor = System.Drawing.Color.Salmon;
+            this.subTitlelabel.Location = new System.Drawing.Point(134, 98);
+            this.subTitlelabel.Name = "subTitlelabel";
+            this.subTitlelabel.Size = new System.Drawing.Size(136, 25);
+            this.subTitlelabel.TabIndex = 1;
+            this.subTitlelabel.Text = "Subtitle label";
+            // 
+            // hero2Points
+            // 
+            this.hero2Points.AutoSize = true;
+            this.hero2Points.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hero2Points.ForeColor = System.Drawing.Color.Salmon;
+            this.hero2Points.Location = new System.Drawing.Point(516, 9);
+            this.hero2Points.Name = "hero2Points";
+            this.hero2Points.Size = new System.Drawing.Size(72, 25);
+            this.hero2Points.TabIndex = 2;
+            this.hero2Points.Text = "Points";
+            // 
+            // hero1Points
+            // 
+            this.hero1Points.AutoSize = true;
+            this.hero1Points.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hero1Points.ForeColor = System.Drawing.Color.Salmon;
+            this.hero1Points.Location = new System.Drawing.Point(12, 9);
+            this.hero1Points.Name = "hero1Points";
+            this.hero1Points.Size = new System.Drawing.Size(72, 25);
+            this.hero1Points.TabIndex = 3;
+            this.hero1Points.Text = "Points";
+            // 
+            // winLabel
+            // 
+            this.winLabel.AutoSize = true;
+            this.winLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winLabel.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.winLabel.Location = new System.Drawing.Point(214, 65);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(135, 33);
+            this.winLabel.TabIndex = 4;
+            this.winLabel.Text = "Win label";
+            this.winLabel.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.SkyBlue;
-            this.ClientSize = new System.Drawing.Size(600, 600);
-            this.Controls.Add(this.scoreLabel);
-            this.Controls.Add(this.timeLabel);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(600, 378);
+            this.Controls.Add(this.winLabel);
+            this.Controls.Add(this.hero1Points);
+            this.Controls.Add(this.hero2Points);
+            this.Controls.Add(this.subTitlelabel);
+            this.Controls.Add(this.titleLabel);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label timeLabel;
-        private System.Windows.Forms.Label scoreLabel;
         private System.Windows.Forms.Timer gameLoop;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subTitlelabel;
+        private System.Windows.Forms.Label hero2Points;
+        private System.Windows.Forms.Label hero1Points;
+        private System.Windows.Forms.Label winLabel;
     }
 }
 
--- CatchGame/Form1.cs
+++ CatchGame/Form1.cs
@@ -7,121 +7,173 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace CatchGame
 {
     public partial class Form1 : Form
     {
-        Rectangle hero = new Rectangle(280, 540, 40, 10);
-        int heroSpeed = 10;
+        Rectangle hero = new Rectangle(350, 200, 10, 20);
+        Rectangle hero2 = new Rectangle(450, 350, 10, 20);
+
+        int heroSpeed = 8;
 
         List<Rectangle> balls = new List<Rectangle>();
+        List<Rectangle> balls2 = new List<Rectangle>();
         List<int> ballSpeeds = new List<int>();
-        List<string> ballColours = new List<string>();
+        List<int> ballSpeeds2 = new List<int>();
 
         int ballSize = 10;
-        int ballSpeed = 7;
+        int time = 378;
+        int hero1score = 0;
+        int hero2score = 0;
 
-        int score = 0;
-        int time = 500;
-
-        bool leftDown = false;
-        bool rightDown = false;
-
-        SolidBrush greenBrush = new SolidBrush(Color.Green);
+        bool upDown = false;
+        bool downDown = false;
+        bool wDown = false;
+        bool sDown = false;
         SolidBrush whiteBrush = new SolidBrush(Color.White);
-        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
-        SolidBrush redBrush = new SolidBrush(Color.Red);
 
         Random randGen = new Random();
+
         int randValue = 0;
 
-        int groundHeight = 50;
-
+        SoundPlayer crash = new SoundPlayer(Properties.Resources.Explosion__sound_effect_);
+        SoundPlayer point = new SoundPlayer(Properties.Resources.mixkit_retro_game_notification_212);
+        SoundPlayer over = new SoundPlayer(Properties.Resources.Game_Over_Sound_Effect);
+        string gameState = "waiting";
         public Form1()
         {
             InitializeComponent();
         }
+        public void GameSetup()
+        {
+            winLabel.Visible = false;
+            gameState = "running";
 
+            gameLoop.Enabled = true;
+
+            hero.X = 200;
+            hero.Y = 350;
+
+            titleLabel.Text = "";
+            subTitlelabel.Text = "";
+            balls.Clear();
+            ballSpeeds.Clear();
+            balls2.Clear();
+            ballSpeeds2.Clear();
+        }
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             switch (e.KeyCode)
             {
-                case Keys.Left:
-                    leftDown = true;
+                case Keys.Up:
+                    upDown = true;
                     break;
-                case Keys.Right:
-                    rightDown = true;
+                case Keys.Down:
+                    downDown = true;
+                    break;
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.Space:
+                    if (gameState == "waiting" || gameState == "over")
+                    {
+                        GameSetup();
+                    }
+                    break;
+                case Keys.Escape:
+                    if (gameState == "waiting" || gameState == "over")
+                    {
+                        this.Close();
+                    }
                     break;
             }
 
         }
-
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
             switch (e.KeyCode)
             {
-                case Keys.Left:
-                    leftDown = false;
+                case Keys.Up:
+                    upDown = false;
                     break;
-                case Keys.Right:
-                    rightDown = false;
+                case Keys.Down:
+                    downDown = false;
+                    break;
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
                     break;
             }
 
         }
-
         private void gameLoop_Tick(object sender, EventArgs e)
         {
-            //move player
-            if (leftDown == true && hero.X > 0)
+            time--;
+            //move player            
+            if (upDown == true)
             {
-                hero.X -= heroSpeed;
+                hero2.Y -= heroSpeed;
             }
-
-            if (rightDown == true && hero.X < this.Width - hero.Width)
+            if (downDown == true && hero2.Y < this.Height - hero2.Height)
             {
-                hero.X += heroSpeed;
+                hero2.Y += heroSpeed;
+            }
+            if (wDown == true)
+            {
+                hero.Y -= heroSpeed;
+            }
+            if (sDown == true && hero.Y < this.Height - hero.Height)
+            {
+                hero.Y += heroSpeed;
             }
 
             //move ball objects
             for (int i = 0; i < balls.Count; i++)
             {
-                int y = balls[i].Y + ballSpeeds[i];
-                balls[i] = new Rectangle(balls[i].X, y, ballSize, ballSize);
+                int x = balls[i].X + ballSpeeds[i];
+                balls[i] = new Rectangle(x, balls[i].Y, ballSize, 5);
             }
-
+            for (int i = 0; i < balls2.Count; i++)
+            {
+                int x = balls2[i].X - ballSpeeds[i];
+                balls2[i] = new Rectangle(x, balls2[i].Y, ballSize, 5);
+            }
             //generate a random value
             randValue = randGen.Next(1, 101);
 
             //generate new ball if it is time
-            if (randValue < 3)
+            if (randValue > 80)
             {
-                balls.Add(new Rectangle(randGen.Next(0, this.Width - ballSize), 0, ballSize, ballSize));
-                ballColours.Add("red");
-                ballSpeeds.Add(16);
+                balls.Add(new Rectangle(0, randGen.Next(0, 340), ballSize, 5));
+                ballSpeeds.Add(3);
             }
-            else if (randValue < 8)
+            if (randValue > 80)
             {
-                balls.Add(new Rectangle(randGen.Next(0, this.Width - ballSize), 0, ballSize, ballSize));
-                ballColours.Add("yellow");
-                ballSpeeds.Add(12);
+                balls2.Add(new Rectangle(this.Width, randGen.Next(0, 340), ballSize, 5));
+                ballSpeeds2.Add(3);
             }
-            else if (randValue < 20)
-            {
-                balls.Add(new Rectangle(randGen.Next(0, this.Width - ballSize), 0, ballSize, ballSize));
-                ballColours.Add("green");
-                ballSpeeds.Add(4);
-            }
-
-            //remove ball if it goes off the screen, (test at y = 400)
+            //remove ball if it goes off the screen
             for (int i = 0; i < balls.Count; i++)
             {
-                if (balls[i].Y >= this.Height)
+                if (balls[i].X >= this.Width)
                 {
                     balls.RemoveAt(i);
-                    ballColours.RemoveAt(i);
                     ballSpeeds.RemoveAt(i);
+                }
+            }
+            for (int i = 0; i < balls2.Count; i++)
+            {
+                if (balls2[i].X <= 0)
+                {
+                    balls2.RemoveAt(i);
+                    ballSpeeds2.RemoveAt(i);
                 }
             }
 
@@ -130,68 +182,89 @@ namespace CatchGame
             {
                 if (hero.IntersectsWith(balls[i]))
                 {
-                    if (ballColours[i] == "green")
-                    {
-                        score = score + 5;  // score += 5;
-                    }
-                    else if (ballColours[i] == "yellow")
-                    {
-                        time = time + 50;
-                    }
-                    else if (ballColours[i] == "red")
-                    {
-                        score = score - 10;
-                    }
-
-                    balls.RemoveAt(i);
-                    ballColours.RemoveAt(i);
-                    ballSpeeds.RemoveAt(i);
-                    
+                    hero.Y = 350;
+                    crash.Play();
+                }
+                else if (hero2.IntersectsWith(balls[i]))
+                {
+                    hero2.Y = 350;
+                    crash.Play();
                 }
             }
+            if (hero.Y <= -10)
+            {
+                hero1score++;
+                hero1Points.Text = $"Points {hero1score}";
+                hero.Y = 350;
+                point.Play();
 
-            //decrease time
-            time--;
+            }
+            if (hero2.Y <= -10)
+            {
+                hero2score++;
+                hero2Points.Text = $"Points {hero2score}";
+                hero2.Y = 350;
+                point.Play();
 
-            //end game if time runs out
+            }
             if (time <= 0)
             {
+                gameState = "over";
                 gameLoop.Enabled = false;
+                over.Play();
             }
-
             Refresh();
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            //update labels
-            timeLabel.Text = $"{time}";
-            scoreLabel.Text = $"Score: {score}";
+            Rectangle timer = new Rectangle(300, 378 - time, 20, time);
 
-            //draw ground
-            e.Graphics.FillRectangle(greenBrush, 0, this.Height - groundHeight,
-                this.Width, groundHeight);
-
-            //draw hero
-            e.Graphics.FillRectangle(whiteBrush, hero);
-
-            //draw balls
-            for (int i = 0; i < balls.Count(); i++)
+            if (gameState == "waiting")
             {
-                if (ballColours[i] == "green")
-                {
-                    e.Graphics.FillEllipse(greenBrush, balls[i]);
-                }
-                else if (ballColours[i] == "red")
-                {
-                    e.Graphics.FillEllipse(redBrush, balls[i]);
-                }
-                else if (ballColours[i] == "yellow")
-                {
-                    e.Graphics.FillEllipse(yellowBrush, balls[i]);
-                }
+                titleLabel.Text = "Space Race";
+                subTitlelabel.Text = "Press Space to Start or Esc to Exit";
             }
+            else if (gameState == "running")
+            {
+                //update labels
+                //
+                //draw hero
+                e.Graphics.FillRectangle(whiteBrush, hero);
+                e.Graphics.FillRectangle(whiteBrush, hero2);
+                e.Graphics.FillRectangle(whiteBrush, timer);
+                for (int i = 0; i < balls.Count(); i++)
+                {
+                    e.Graphics.FillRectangle(whiteBrush, balls[i]);
+                }
+                for (int i = 0; i < balls2.Count(); i++)
+                {
+                    e.Graphics.FillRectangle(whiteBrush, balls2[i]);
+                }
 
+            }
+            else if (gameState == "over")
+            {
+                subTitlelabel.Text = "Press Space to Start or Esc to Exit";
+
+                time = 500;
+                winLabel.Visible = true;
+                if (hero1score > hero2score)
+                {
+                    winLabel.Text = "Hero 1 Wins";
+                }
+                else if (hero1score < hero2score)
+                {
+                    winLabel.Text = "Hero 2 Wins";
+                }
+                else
+                {
+                    winLabel.Text = "Tie";
+                }
+                hero1score = 0;
+                hero2score = 0;
+                hero1Points.Text = $"Points {hero1score}";
+            }
         }
     }
 }
